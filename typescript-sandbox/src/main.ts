879a27b54ec86a9da9a4ce29fb021a15fa012208
Tony <antoniodltorregarcia@gmail.com>
2026-05-14 12:00:14 +0200

    Cambios añadidos

---
+++ typescript-sandbox/src/main.ts
@@ -0,0 +1,22 @@
+// Cena de cumpleaños entre amigos
+// Tienes un grupo de 6 amigos y quieres invitarlos a cenar por tu cumpleaños.
+
+// Solo puedes permitirte invitar a las bebidas, ya que estás un poco ajustado de dinero.
+// Tienes un ticket de cena que cuesta 120 € y en el que ya se incluyen las bebidas por un valor de 18 €.
+// Calcula cuánto tendría que pagar cada comensal para dividir los costos de manera equitativa.
+// Utiliza JavaScript para hacer el cálculo y mostrar el resultado por consola.
+
+//Datos con los que trabajamos
+console.log({
+    total: '120€',
+    bebidas: '18€'
+})
+
+//Precio total sin las bebidas
+console.log(120 - 18); //102
+
+//Dividimos el precio sin las bebidas
+console.log(102 / 6); //17
+
+//Dividimos el precio sin las bebidas incluyendonos
+console.log(102 / 7); //14.57
